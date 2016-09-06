49072744d992aab8d18e0712b7311f324a76811a
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-06 15:06:55 -0500

    Create colored box on each button click

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,15 @@ console.log('scripts.js sourced!');
 
 
 /// == Function Declarations == ///
-
+function createBox(){
+  var boxColor = $(this).data('color');
+  $('#boxPlayground').append('<div class="color-cube '+boxColor+'"></div>');
+}
 
 /// == JavaScript == ///
 
 $(document).ready(function(){
   console.log('Document ready!');
 
-  
+  $('.color-button').on('click',createBox);
 }); // end document ready
