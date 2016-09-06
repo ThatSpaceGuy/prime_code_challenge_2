b236e6dd1c8e7f6608665c2f2305e33dfee753b0
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-06 15:47:59 -0500

    Ver2.0 - Hard mode complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,27 +9,43 @@ var boxCount = {
 };
 
 /// == Function Declarations == ///
-function createBox(){
-  console.log('in createBox');
+function addRemoveBox(){
+  console.log('in addRemoveBox');
+  var addBox = true; // assume that a box is being added
   // get color of button clicked
   var boxColor = $(this).data('color');
-  // add box of boxColor to the boxPlayground
-  $('#boxPlayground').append('<div class="color-cube '+boxColor+'"></div>');
+
+  if ($(this).hasClass('color-button')){
+    // add box of boxColor to the boxPlayground
+    $('#boxPlayground').append('<div class="clickable color-cube '+boxColor+
+    '" data-color="'+boxColor+'"></div>');
+  } else {
+    // arriving here means that a box was clicked and not a button
+    addBox = false; // change flag because box is being removed
+    // remove the clicked box
+    $(this).remove();
+  }
   // call function to update the count
-  updateCount(boxColor);
+  updateCount(boxColor, addBox);
 }
 
-function updateCount(newColor){
-  // increment the count of a specific color
-  boxCount[newColor]++;
+function updateCount(thisColor, addingBox){
+  if (addingBox) {
+    // increment the count of a specific color
+    boxCount[thisColor]++;
+  } else {
+    // decrement the count of a specific color
+    boxCount[thisColor]--;
+  }
+
   // display new count on DOM
-  $('#'+newColor).html('Total '+newColor+': '+boxCount[newColor]);
+  $('#'+thisColor).html('Total '+thisColor+': '+boxCount[thisColor]);
 }
 
 /// == JavaScript == ///
 $(document).ready(function(){
   console.log('Document ready!');
 
-  // call createBox whenever a color-button is clicked
-  $('.color-button').on('click',createBox);
+  // call addRemoveBox whenever something .clickable is clicked
+  $('body').on('click','.clickable',addRemoveBox);
 }); // end document ready
